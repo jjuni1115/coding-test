35b47859e56ff231188027f5df7b364c993f09d2
jjuni1115 <junyege@naver.com>
2025-05-08 17:02:26 +0900

    20250508 문제풀이

---
+++ src/C20250508_1.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.List;
+
+public class C20250508_1 {
+
+
+    public static void main(String[] args) {
+
+
+        String[] phone_book = new String[]{"123", "456", "789"};
+        System.out.println(solution(phone_book));
+
+    }
+
+
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        HashSet<String> phoneSet = new HashSet<>(List.of(phone_book));
+
+
+        for (String phone : phone_book) {
+            for (int i = 1; i < phone.length(); i++) {
+                if (phoneSet.contains(phone.substring(0, i))) {
+                    return false;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+}

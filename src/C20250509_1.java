e2ce632cf676dfc5a3317b2fba1f40fbe593f1c1
jjuni1115 <junyege@naver.com>
2025-05-09 17:26:49 +0900

    20250509 문제풀이

---
+++ src/C20250509_1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class C20250509_1 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1,1,3,3,0,1,1};
+        solution(arr);
+
+    }
+
+    public static int[] solution(int []arr) {
+        List<Integer> answer = new ArrayList<>();
+
+        answer.add(arr[0]);
+
+        int temp = arr[0];
+
+        for (int i : arr){
+            if(temp == i){
+                continue;
+            }else{
+                answer.add(i);
+                temp=i;
+            }
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+
+
+}
